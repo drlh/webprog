c0415f9f10599849b72a5205d9a92a544df15f57
Leo Hellwich <leo.hellwich@lhellwich.com>
2015-03-17 14:53:20 +0100

    .

--- Webprog/WebContent/jsp/preis.jsp
+++ Webprog/WebContent/jsp/preis.jsp
@@ -1,3 +1,6 @@
+<%@ page import="java.sql.*" %>
+<%@ page import="java.io.*" %>
+<%@ page import="java.util.*" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 </html>
---
+++ Webprog/src/web/Preisanfrage.java
@@ -0,0 +1,21 @@
+package web;
+
+import java.sql.*;
+import java.util.*;
+import java.io.*;
+
+public class Preisanfrage {
+	
+	
+	public Preisanfrage() 
+	{
+		
+	}
+	
+	public void datenSpeichern(){
+		
+	}
+	
+	
+	
+}
