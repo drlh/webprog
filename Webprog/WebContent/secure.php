405863c7e0f1b351818da957560762bc12b6658a
Leo Hellwich <leo.hellwich@lhellwich.com>
2015-03-19 15:09:55 +0100

    123

--- Webprog/WebContent/secure.php
+++ Webprog/WebContent/secure.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION["email"])) {
 else {
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-	curl_setopt($ch, CURLOPT_URL, '../../../tomcat/webapps/vereinjsp/start.jsp'); //PFAD HIER ÄNDERN?
+	curl_setopt($ch, CURLOPT_URL, 'http://localhost/Webprog/jsp/start.jsp'); 
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
